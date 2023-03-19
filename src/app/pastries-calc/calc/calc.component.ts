e5de87411c821b90f9a9fa0022badbc31a490677
Roger Terrill <lpaddikt@gmail.com>
2023-03-19 14:50:46 -0700

    Move calculator to own component

---
+++ src/app/pastries-calc/calc/calc.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.sass']
+})
+export class CalcComponent {
+  title = 'Baking Calc';
+}
